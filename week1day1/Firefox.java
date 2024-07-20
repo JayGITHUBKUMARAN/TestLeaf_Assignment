a797d3065e5fca04153a875fd8148ef6ade29ecd
JayGITHUBKUMARAN <sj.kumaran05@gmail.com>
2024-07-20 23:27:33 +0530

    Add files via upload
    Test Leaf - Day 1 - Class Assignments.

---
+++ week1day1/Firefox.java
@@ -0,0 +1,22 @@
+package week1day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		float browserVersion=100.2f;
+		String browserName="firefox";
+		boolean isVisible=true;
+		int releaseYear=1998;
+		char browserlogo='f';
+		
+	System.out.println("Browser Version:"+browserVersion);	
+	System.out.println("Browser Name:"+browserName);
+	System.out.println("Browser Visible in:"+isVisible);
+	System.out.println("Browser Release year is:"+releaseYear);
+	System.out.println("Browser logo is:"+browserlogo);
+	
+	
+
+	}
+
+}
---
+++ week1day1/TestLeaf.java
@@ -0,0 +1,17 @@
+package week1day1;
+
+public class TestLeaf {
+	
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome");
+		System.out.println("Welcome to Test leaf");
+		int nooftabsopen=3;
+		boolean checkBrowserloaded=true;
+		char checkfisrtletterofthebrowser='C';
+		
+		
+	}
+
+}
